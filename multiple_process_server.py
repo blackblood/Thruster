4fada503402557003f254ca92f4b8c019c76c66b
Akshay Takkar <akshay.takkar@coverfoxmail.com>
2019-05-12 13:23:24 +0530

    formating using black

--- multiple_process_server.py
+++ multiple_process_server.py
@@ -8,83 +8,97 @@ import sys
 
 from wsgi_server import WSGIServer as Worker
 from datetime import datetime
-from http_utils.base import get_http_status_text, get_ext_from_mime_type, get_mime_type_from_ext
+from http_utils.base import (
+    get_http_status_text,
+    get_ext_from_mime_type,
+    get_mime_type_from_ext,
+)
 from pysigset import suspended_signals
 
-SERVER_ADDRESS = (HOST, PORT) = '127.0.0.1', 8888
+SERVER_ADDRESS = (HOST, PORT) = "127.0.0.1", 8888
 REQUEST_QUEUE_SIZE = 5
 
-class MasterWorker():
-	def __init__(self):
-		self.workers = list()
-		self.server_name = socket.getfqdn(HOST)
-		self.server_port = PORT
-		self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		# self.listen_socket.setblocking(False)
-		ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
-		ssl_context.load_cert_chain(certfile='certificate.pem', keyfile='key.pem')
-		ssl_context.set_alpn_protocols(['h2', 'spdy/2', 'http/1.1'])
-		self.context = ssl_context
-		self.listen_socket.bind(SERVER_ADDRESS)
-		self.listen_socket.listen(REQUEST_QUEUE_SIZE)
-		self.pid = os.getpid()
-		print(('Serving HTTP on port {port} ...'.format(port=PORT)))
-		print(('Parent PID (PPID): {pid}\n'.format(pid=os.getpid())))
-  
-	def shutdown_workers(self, signum, frame):
-		print(("shutting down pid: %d" % os.getpid()))
-		os._exit(0)
-	
-	async def create_worker_pool(self):
-		sys.path.insert(0, 'mysite')
-		module = __import__('mysite', globals(), locals(), ['asgi'], 0)
-		self.application = module.asgi.application
-		signal.signal(signal.SIGINT, self.shutdown_workers)
-		signal.signal(signal.SIGCHLD, self.restart_worker)
-		for _ in range(5):
-			await self.create_worker()
-		
-		with suspended_signals(signal.SIGINT):
-			try:
-				while os.wait() != -1:
-					print("child process terminated")
-					continue
-			except OSError:
-				pass
-		print("Exiting...")
-		print("Bye Bye!")
-    
-	async def create_worker(self):
-		pid = os.fork()
-		if pid == 0:
-			worker = Worker(self.server_name, self.server_port)
-			worker.set_app(self.application)
-			while True:
-				try:
-					client_connection, client_address = self.listen_socket.accept()
-					worker.client_connection = self.context.wrap_socket(client_connection, server_side=True)
-					print(("selected alpn protocol: %s" % worker.client_connection.selected_alpn_protocol()))
-					print(("handled by pid: %d" % os.getpid()))
-					await worker.handle_request()
-				except Exception:
-					print((traceback.format_exc()))
-			os._exit(0)
-		else:
-			print(("Created worker with pid: %d" % pid))
-			return pid
 
-	def restart_worker(self, signum, frame):
-		if os.WIFEXITED(signum):
-			print("Restarting worker...")
-			self.create_worker()
-		if os.WIFSIGNALED(signum):
-			print("signalled")
+class MasterWorker:
+    def __init__(self):
+        self.workers = list()
+        self.server_name = socket.getfqdn(HOST)
+        self.server_port = PORT
+        self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # self.listen_socket.setblocking(False)
+        ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+        ssl_context.load_cert_chain(certfile="certificate.pem", keyfile="key.pem")
+        ssl_context.set_alpn_protocols(["h2", "spdy/2", "http/1.1"])
+        self.context = ssl_context
+        self.listen_socket.bind(SERVER_ADDRESS)
+        self.listen_socket.listen(REQUEST_QUEUE_SIZE)
+        self.pid = os.getpid()
+        print(("Serving HTTP on port {port} ...".format(port=PORT)))
+        print(("Parent PID (PPID): {pid}\n".format(pid=os.getpid())))
+
+    def shutdown_workers(self, signum, frame):
+        print(("shutting down pid: %d" % os.getpid()))
+        os._exit(0)
+
+    async def create_worker_pool(self):
+        sys.path.insert(0, "mysite")
+        module = __import__("mysite", globals(), locals(), ["asgi"], 0)
+        self.application = module.asgi.application
+        signal.signal(signal.SIGINT, self.shutdown_workers)
+        signal.signal(signal.SIGCHLD, self.restart_worker)
+        for _ in range(5):
+            await self.create_worker()
+
+        with suspended_signals(signal.SIGINT):
+            try:
+                while os.wait() != -1:
+                    print("child process terminated")
+                    continue
+            except OSError:
+                pass
+        print("Exiting...")
+        print("Bye Bye!")
+
+    async def create_worker(self):
+        pid = os.fork()
+        if pid == 0:
+            worker = Worker(self.server_name, self.server_port)
+            worker.set_app(self.application)
+            while True:
+                try:
+                    client_connection, client_address = self.listen_socket.accept()
+                    worker.client_connection = self.context.wrap_socket(
+                        client_connection, server_side=True
+                    )
+                    print(
+                        (
+                            "selected alpn protocol: %s"
+                            % worker.client_connection.selected_alpn_protocol()
+                        )
+                    )
+                    print(("handled by pid: %d" % os.getpid()))
+                    await worker.handle_request()
+                except Exception:
+                    print((traceback.format_exc()))
+            os._exit(0)
+        else:
+            print(("Created worker with pid: %d" % pid))
+            return pid
+
+    def restart_worker(self, signum, frame):
+        if os.WIFEXITED(signum):
+            print("Restarting worker...")
+            self.create_worker()
+        if os.WIFSIGNALED(signum):
+            print("signalled")
+
 
 def serve_forever():
-	master_worker = MasterWorker()
-	event_loop = asyncio.get_event_loop()
-	event_loop.run_until_complete(master_worker.create_worker_pool())
+    master_worker = MasterWorker()
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(master_worker.create_worker_pool())
 
-if __name__ == '__main__':
-  	serve_forever()
+
+if __name__ == "__main__":
+    serve_forever()
--- wsgi_server.py
+++ wsgi_server.py
@@ -17,207 +17,242 @@ from http2.frames.rst_frame import RstStreamFrame
 from http2.frames import utils
 from http2.stream import Stream
 
+
 class WSGIServer(object):
-	address_family = socket.AF_INET
-	socket_type = socket.SOCK_STREAM
-	request_queue_size = 1
+    address_family = socket.AF_INET
+    socket_type = socket.SOCK_STREAM
+    request_queue_size = 1
 
-	def __init__(self, server_name, server_port):
-		self.server_name = server_name
-		self.server_port = server_port
-		self.client_connection = None
-		self.header_encoder = Encoder()
-		self.header_decoder = Decoder()
-		self.request_data = ""
-		self.headers_set = []
-		self.connection_established = False
-		self.response_queue = queue.Queue()
+    def __init__(self, server_name, server_port):
+        self.server_name = server_name
+        self.server_port = server_port
+        self.client_connection = None
+        self.header_encoder = Encoder()
+        self.header_decoder = Decoder()
+        self.request_data = ""
+        self.headers_set = []
+        self.connection_established = False
+        self.response_queue = queue.Queue()
 
-	def set_app(self, application):
-		self.application = application
-	
-	async def asgi_more_data(self):
-		request_data = self.client_connection.recv(4096)
-		frame = self.parse_request(request_data)
-		if isinstance(frame, DataFrame):
-			asgi_event = {"type": "http.request", "body": frame.body}
-			if frame.end_stream:
-				asgi_event["more_body"] = False
-			else:
-				asgi_event["more_body"] = True
-			
-			return asgi_event
-	
-	async def trigger_asgi_application(self):
-		return { "type": "http.request", "body": b"", "more_body": False }
+    def set_app(self, application):
+        self.application = application
 
-	async def handle_request(self):
-		try:
-			while True:
-				self.request_data = self.client_connection.recv(4096)
-				self.frame = self.parse_request(self.request_data)
-				if isinstance(self.frame, SettingsFrame):
-					self.client_connection.sendall(SettingsFrame.get_acknowledgement_frame().bytes)
-					self.connection_established = True
-				elif isinstance(self.frame, HeadersFrame):
-					if self.frame.end_stream:
-						current_stream = Stream(
-							self.connection_settings,
-							self.header_encoder,
-							self.header_decoder,
-							self.client_connection
-						)
-						current_stream.stream_id = self.frame.stream_id
-						asgi_scope = self.get_asgi_event_dict(self.frame)
-						current_stream.asgi_app = self.application(asgi_scope)
-						await current_stream.asgi_app(self.trigger_asgi_application, current_stream.send_response)
-					else:
-						self.asgi_scope = self.get_asgi_event_dict(self.frame)
-				elif isinstance(self.frame, ContinuationFrame):
-					if self.frame.end_stream:
-						current_stream = Stream(
-							self.connection_settings,
-							self.header_encoder,
-							self.header_decoder,
-							self.client_connection
-						)
-						current_stream.asgi_app = self.application(self.asgi_scope)
-						current_stream.stream_id = self.frame.stream_id
-						await current_stream.asgi_app(self.trigger_asgi_application, current_stream.send_response)
-					else:
-						self.asgi_scope["headers"].extend([[k,v] for k,v in self.frame.headers.iteritems()])
-				elif isinstance(self.frame, DataFrame):
-					pass
-				elif isinstance(self.frame, RstStreamFrame):
-					print("RSTFrame received with error: (%d, %s)" % (self.frame.error_code, self.frame.description))
-				else:
-					pass
-		except Exception:
-			print("Error occurred in handle_request")
-			print((traceback.format_exc()))
-	
-	def parse_request(self, raw_data):
-		if raw_data:
-			if not self.connection_established:
-				raw_data = raw_data[24:]
-			if not raw_data:
-				bits = {}
-				self.connection_settings = SettingsFrame().read(raw_data)
-				return self.connection_settings
-			bits = bitstring.ConstBitStream(bytes=raw_data)
-			frame_length = bits.read("uint:24")
-			frame_type = bits.read("hex:8")
-			if frame_type == '04':
-				self.connection_settings = SettingsFrame().read(bits)
-				return self.connection_settings
-			elif frame_type == '01':
-				header_frame = HeadersFrame(self.header_encoder, self.header_decoder)
-				header_frame.read(bits)
-				return header_frame
-			elif frame_type == '03':
-				rst_frame = RstStreamFrame()
-				rst_frame.read(bits)
-				return rst_frame
-			elif frame_type == '09':
-				cont_frame = ContinuationFrame()
-				cont_frame.read(bits)
-				return cont_frame
-			elif frame_type == '00':
-				data_frame = DataFrame()
-				data_frame.read(bits)
-				return data_frame
+    async def asgi_more_data(self):
+        request_data = self.client_connection.recv(4096)
+        frame = self.parse_request(request_data)
+        if isinstance(frame, DataFrame):
+            asgi_event = {"type": "http.request", "body": frame.body}
+            if frame.end_stream:
+                asgi_event["more_body"] = False
+            else:
+                asgi_event["more_body"] = True
 
-	def set_env(self):
-		env = {}
+            return asgi_event
 
-		env['wsgi.version'] = (1,0)
-		env['wsgi.url_scheme'] = 'https'
-		env['wsgi.input'] = io.StringIO(self.request_data)
-		env['wsgi.errors'] = sys.stderr
-		env['wsgi.multithread'] = False
-		env['wsgi.multiprocess'] = False
-		env['wsgi.run_once'] = False
-		env['REQUEST_METHOD'] = self.frame.get_method()
-		env['PATH_INFO'] = self.frame.get_path()
-		env['SERVER_NAME'] = self.server_name
-		env['SERVER_PORT'] = str(self.server_port)
+    async def trigger_asgi_application(self):
+        return {"type": "http.request", "body": b"", "more_body": False}
 
-		return env
-	
-	def get_asgi_event_dict(self, frame):
-		event_dict = {
-			"type": "http",
-			"asgi": {
-				"version": "2.0",
-				"spec_version": "2.1"
-			},
-			"http_version": "2",
-			"method": frame.get_method(),
-			"scheme": "https",
-			"path": frame.get_path(),
-			"query_string": "",
-			"headers": [[k.encode("utf-8"),v.encode("utf-8")] for k,v in frame.headers.items()],
-		}
-		return event_dict
+    async def handle_request(self):
+        try:
+            while True:
+                self.request_data = self.client_connection.recv(4096)
+                self.frame = self.parse_request(self.request_data)
+                if isinstance(self.frame, SettingsFrame):
+                    self.client_connection.sendall(
+                        SettingsFrame.get_acknowledgement_frame().bytes
+                    )
+                    self.connection_established = True
+                elif isinstance(self.frame, HeadersFrame):
+                    if self.frame.end_stream:
+                        current_stream = Stream(
+                            self.connection_settings,
+                            self.header_encoder,
+                            self.header_decoder,
+                            self.client_connection,
+                        )
+                        current_stream.stream_id = self.frame.stream_id
+                        asgi_scope = self.get_asgi_event_dict(self.frame)
+                        current_stream.asgi_app = self.application(asgi_scope)
+                        await current_stream.asgi_app(
+                            self.trigger_asgi_application, current_stream.send_response
+                        )
+                    else:
+                        self.asgi_scope = self.get_asgi_event_dict(self.frame)
+                elif isinstance(self.frame, ContinuationFrame):
+                    if self.frame.end_stream:
+                        current_stream = Stream(
+                            self.connection_settings,
+                            self.header_encoder,
+                            self.header_decoder,
+                            self.client_connection,
+                        )
+                        current_stream.asgi_app = self.application(self.asgi_scope)
+                        current_stream.stream_id = self.frame.stream_id
+                        await current_stream.asgi_app(
+                            self.trigger_asgi_application, current_stream.send_response
+                        )
+                    else:
+                        self.asgi_scope["headers"].extend(
+                            [[k, v] for k, v in self.frame.headers.iteritems()]
+                        )
+                elif isinstance(self.frame, DataFrame):
+                    pass
+                elif isinstance(self.frame, RstStreamFrame):
+                    print(
+                        "RSTFrame received with error: (%d, %s)"
+                        % (self.frame.error_code, self.frame.description)
+                    )
+                else:
+                    pass
+        except Exception:
+            print("Error occurred in handle_request")
+            print((traceback.format_exc()))
 
-	def start_response(self, status, response_headers, exc_info=None):
-		server_headers = {
-			'Date': datetime.datetime.now().strftime('%a, %d %b %Y %H:%M:%S GMT'),
-			'Server': 'WSGIServer 0.2'
-		}
+    def parse_request(self, raw_data):
+        if raw_data:
+            if not self.connection_established:
+                raw_data = raw_data[24:]
+            if not raw_data:
+                bits = {}
+                self.connection_settings = SettingsFrame().read(raw_data)
+                return self.connection_settings
+            bits = bitstring.ConstBitStream(bytes=raw_data)
+            frame_length = bits.read("uint:24")
+            frame_type = bits.read("hex:8")
+            if frame_type == "04":
+                self.connection_settings = SettingsFrame().read(bits)
+                return self.connection_settings
+            elif frame_type == "01":
+                header_frame = HeadersFrame(self.header_encoder, self.header_decoder)
+                header_frame.read(bits)
+                return header_frame
+            elif frame_type == "03":
+                rst_frame = RstStreamFrame()
+                rst_frame.read(bits)
+                return rst_frame
+            elif frame_type == "09":
+                cont_frame = ContinuationFrame()
+                cont_frame.read(bits)
+                return cont_frame
+            elif frame_type == "00":
+                data_frame = DataFrame()
+                data_frame.read(bits)
+                return data_frame
 
-		server_headers.update({k: v for k, v in response_headers})
-		self.headers_set = status, server_headers
+    def set_env(self):
+        env = {}
 
-	def finish_response(self, result):
-		try:
-			status, response_headers = self.headers_set
-			response_headers[':status'] = status.split(" ")[0]
-			headers_frame = HeadersFrame(self.connection_settings, self.header_encoder, self.header_decoder)
-			encoded_headers = self.header_encoder.encode(HeadersFrame.normalize_header_fields(response_headers))
+        env["wsgi.version"] = (1, 0)
+        env["wsgi.url_scheme"] = "https"
+        env["wsgi.input"] = io.StringIO(self.request_data)
+        env["wsgi.errors"] = sys.stderr
+        env["wsgi.multithread"] = False
+        env["wsgi.multiprocess"] = False
+        env["wsgi.run_once"] = False
+        env["REQUEST_METHOD"] = self.frame.get_method()
+        env["PATH_INFO"] = self.frame.get_path()
+        env["SERVER_NAME"] = self.server_name
+        env["SERVER_PORT"] = str(self.server_port)
 
-			if len(encoded_headers) > self.connection_settings.max_frame_size:
-				self.client_connection.sendall(
-					headers_frame.write(
-						flags={
-							'end_stream': '0', 'end_headers': '0', 'padded': '0', 'priority': '1'
-						},
-						headers_block_fragment=encoded_headers[0:self.connection_settings.max_frame_size]
-					).bytes
-				)
-				for chunk, is_last in utils.get_chunks(encoded_headers, self.connection_settings.max_frame_size):
-					self.client_connection.sendall(
-						ContinuationFrame().write(
-							flags={'end_headers': '1' if is_last else '0'},
-							headers_block_fragment=chunk
-						).bytes
-					)
-			else:
-				self.client_connection.sendall(
-					headers_frame.write(
-						flags={
-							'end_stream': '0', 'end_headers': '1', 'padded': '0', 'priority': '1'
-						},
-						headers_block_fragment=encoded_headers[0:self.connection_settings.max_frame_size]
-					).bytes
-				)
-			
-			response = ""
-			for data in result:
-				response += data
-			
-			data_frame = DataFrame(self.connection_settings)
-			try:
-				for chunk, is_last in utils.get_chunks(bytearray(response, "utf-8"), self.connection_settings.max_frame_size):
-					self.client_connection.sendall(
-						data_frame.write(
-							flags={'end_stream': '1' if is_last else '0', 'padded': '0'},
-							response_body=chunk
-						).bytes
-					)
-			except OSError as e:
-				print(e)
-		except Exception as exp:
-			print((traceback.format_exc()))
-			print(exp)
-			print("Exception raised in finish response")
+        return env
+
+    def get_asgi_event_dict(self, frame):
+        event_dict = {
+            "type": "http",
+            "asgi": {"version": "2.0", "spec_version": "2.1"},
+            "http_version": "2",
+            "method": frame.get_method(),
+            "scheme": "https",
+            "path": frame.get_path(),
+            "query_string": "",
+            "headers": [
+                [k.encode("utf-8"), v.encode("utf-8")] for k, v in frame.headers.items()
+            ],
+        }
+        return event_dict
+
+    def start_response(self, status, response_headers, exc_info=None):
+        server_headers = {
+            "Date": datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S GMT"),
+            "Server": "WSGIServer 0.2",
+        }
+
+        server_headers.update({k: v for k, v in response_headers})
+        self.headers_set = status, server_headers
+
+    def finish_response(self, result):
+        try:
+            status, response_headers = self.headers_set
+            response_headers[":status"] = status.split(" ")[0]
+            headers_frame = HeadersFrame(
+                self.connection_settings, self.header_encoder, self.header_decoder
+            )
+            encoded_headers = self.header_encoder.encode(
+                HeadersFrame.normalize_header_fields(response_headers)
+            )
+
+            if len(encoded_headers) > self.connection_settings.max_frame_size:
+                self.client_connection.sendall(
+                    headers_frame.write(
+                        flags={
+                            "end_stream": "0",
+                            "end_headers": "0",
+                            "padded": "0",
+                            "priority": "1",
+                        },
+                        headers_block_fragment=encoded_headers[
+                            0 : self.connection_settings.max_frame_size
+                        ],
+                    ).bytes
+                )
+                for chunk, is_last in utils.get_chunks(
+                    encoded_headers, self.connection_settings.max_frame_size
+                ):
+                    self.client_connection.sendall(
+                        ContinuationFrame()
+                        .write(
+                            flags={"end_headers": "1" if is_last else "0"},
+                            headers_block_fragment=chunk,
+                        )
+                        .bytes
+                    )
+            else:
+                self.client_connection.sendall(
+                    headers_frame.write(
+                        flags={
+                            "end_stream": "0",
+                            "end_headers": "1",
+                            "padded": "0",
+                            "priority": "1",
+                        },
+                        headers_block_fragment=encoded_headers[
+                            0 : self.connection_settings.max_frame_size
+                        ],
+                    ).bytes
+                )
+
+            response = ""
+            for data in result:
+                response += data
+
+            data_frame = DataFrame(self.connection_settings)
+            try:
+                for chunk, is_last in utils.get_chunks(
+                    bytearray(response, "utf-8"),
+                    self.connection_settings.max_frame_size,
+                ):
+                    self.client_connection.sendall(
+                        data_frame.write(
+                            flags={
+                                "end_stream": "1" if is_last else "0",
+                                "padded": "0",
+                            },
+                            response_body=chunk,
+                        ).bytes
+                    )
+            except OSError as e:
+                print(e)
+        except Exception as exp:
+            print((traceback.format_exc()))
+            print(exp)
+            print("Exception raised in finish response")
