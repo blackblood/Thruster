9b5d35f2dc29636235576826de8bbcbb1d55be6e
Akshay Takkar <akshay.takkar@coverfoxmail.com>
2019-07-14 18:42:40 +0530

    getting arguments from command line

--- multiple_process_server.py
+++ multiple_process_server.py
@@ -16,19 +16,18 @@ from http_utils.base import (
 )
 from pysigset import suspended_signals
 
-SERVER_ADDRESS = (HOST, PORT) = "127.0.0.1", 8888
-REQUEST_QUEUE_SIZE = 5
-
-
 class MasterWorker:
-    def __init__(self):
+    def __init__(self, app, host, port, request_queue_size):
         ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
         ssl_context.load_cert_chain(certfile="certificate.pem", keyfile="key.pem")
         ssl_context.set_alpn_protocols(["h2", "spdy/2", "http/1.1"])
         self.ssl_context = ssl_context
         self.pid = os.getpid()
-        print(("Serving HTTP on port {port} ...".format(port=PORT)))
-        print(("Parent PID (PPID): {pid}\n".format(pid=os.getpid())))
+        self.APP = app
+        self.HOST = host
+        self.PORT = port
+        self.REQUEST_QUEUE_SIZE = request_queue_size
+        print(("Serving HTTP on port {port} ...".format(port=self.PORT)))
 
     def shutdown_workers(self, signum, frame):
         print(("shutting down pid: %d" % os.getpid()))
@@ -36,24 +35,13 @@ class MasterWorker:
 
     async def run(self):
         signal.signal(signal.SIGINT, self.shutdown_workers)
-
-        server = await asyncio.start_server(self.set_up_producer_consumer, HOST, PORT, family=socket.AF_INET, ssl=self.ssl_context, reuse_address=True)
+        server = await asyncio.start_server(self.set_up_producer_consumer, self.HOST, self.PORT, family=socket.AF_INET, ssl=self.ssl_context, reuse_address=True)
         await server.wait_closed()
-
-        with suspended_signals(signal.SIGINT):
-            try:
-                while os.wait() != -1:
-                    print("child process terminated")
-                    continue
-            except OSError:
-                pass
-        print("Exiting...")
-        print("Bye Bye!")
     
     async def set_up_producer_consumer(self, stream_reader, stream_writer):
-        worker = Worker(socket.getfqdn(HOST), PORT)
-        sys.path.insert(0, self.arguments.app[0])
-        module = __import__(self.arguments.app[0], globals(), locals(), ["asgi"], 0)
+        worker = Worker(socket.getfqdn(self.HOST), self.PORT)
+        sys.path.insert(0, self.APP)
+        module = __import__(self.APP, globals(), locals(), ["asgi"], 0)
         worker.application = module.asgi.application
         worker.frame_queue = asyncio.Queue()
         worker.socket_reader = stream_reader
@@ -64,10 +52,13 @@ class MasterWorker:
     
 
 def serve_forever():
-    master_worker = MasterWorker()
     parser = argparse.ArgumentParser(prog="HTTP/2 ASGI compatible web server for python.")
     parser.add_argument("--app", nargs=1, help="path to your ASGI Application")
-    master_worker.arguments = parser.parse_args()
+    parser.add_argument("--host", nargs=1, help="Host IP Address", default="127.0.0.1")
+    parser.add_argument("--port", nargs=1, help="Port Number", default=8888)
+    parser.add_argument("--queue-size", nargs=1, help="Request Queue Size", default=15)
+    arguments = parser.parse_args()
+    master_worker = MasterWorker(arguments.app[0], arguments.host, arguments.port, arguments.queue_size)
     asyncio.run(master_worker.run())
 
 if __name__ == "__main__":
