b193d786fcb52afa68936df7cd6e75d3f821e84d
Akshay Takkar <akshay.takkar@coverfoxmail.com>
2019-07-14 17:33:01 +0530

    passing application as an argument

--- multiple_process_server.py
+++ multiple_process_server.py
@@ -5,6 +5,7 @@ import signal
 import traceback
 import asyncio
 import sys
+import argparse
 
 from wsgi_server import WSGIServer as Worker
 from datetime import datetime
@@ -18,21 +19,9 @@ from pysigset import suspended_signals
 SERVER_ADDRESS = (HOST, PORT) = "127.0.0.1", 8888
 REQUEST_QUEUE_SIZE = 5
 
-async def set_up_producer_consumer(stream_reader, stream_writer):
-    worker = Worker(socket.getfqdn(HOST), PORT)
-    sys.path.insert(0, "mysite")
-    module = __import__("mysite", globals(), locals(), ["asgi"], 0)
-    worker.application = module.asgi.application
-    worker.frame_queue = asyncio.Queue()
-    worker.socket_reader = stream_reader
-    worker.socket_writer = stream_writer
-    t1 = asyncio.create_task(worker.get_frame())
-    t2 = asyncio.create_task(worker.handle_request())
-    await asyncio.wait([t1, t2])
 
 class MasterWorker:
     def __init__(self):
-        self.workers = list()
         ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
         ssl_context.load_cert_chain(certfile="certificate.pem", keyfile="key.pem")
         ssl_context.set_alpn_protocols(["h2", "spdy/2", "http/1.1"])
@@ -48,7 +37,7 @@ class MasterWorker:
     async def run(self):
         signal.signal(signal.SIGINT, self.shutdown_workers)
 
-        server = await asyncio.start_server(set_up_producer_consumer, HOST, PORT, family=socket.AF_INET, ssl=self.ssl_context, reuse_address=True)
+        server = await asyncio.start_server(self.set_up_producer_consumer, HOST, PORT, family=socket.AF_INET, ssl=self.ssl_context, reuse_address=True)
         await server.wait_closed()
 
         with suspended_signals(signal.SIGINT):
@@ -61,9 +50,24 @@ class MasterWorker:
         print("Exiting...")
         print("Bye Bye!")
     
+    async def set_up_producer_consumer(self, stream_reader, stream_writer):
+        worker = Worker(socket.getfqdn(HOST), PORT)
+        sys.path.insert(0, self.arguments.app[0])
+        module = __import__(self.arguments.app[0], globals(), locals(), ["asgi"], 0)
+        worker.application = module.asgi.application
+        worker.frame_queue = asyncio.Queue()
+        worker.socket_reader = stream_reader
+        worker.socket_writer = stream_writer
+        t1 = asyncio.create_task(worker.get_frame())
+        t2 = asyncio.create_task(worker.handle_request())
+        await asyncio.wait([t1, t2])
+    
 
 def serve_forever():
     master_worker = MasterWorker()
+    parser = argparse.ArgumentParser(prog="HTTP/2 ASGI compatible web server for python.")
+    parser.add_argument("--app", nargs=1, help="path to your ASGI Application")
+    master_worker.arguments = parser.parse_args()
     asyncio.run(master_worker.run())
 
 if __name__ == "__main__":
